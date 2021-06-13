f7d950921e648ebcdab603dc4c42334e15893db1
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-14 00:04:49 +0530

    upto 2.01

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -2,49 +2,65 @@ import React, { Component } from 'react'
 import { getMovies } from './MovieService'
 
 export default class Movies extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
-        this.state={
-            movies:getMovies(),
+        this.state = {
+            movies: getMovies(),
+            currentSearchText:''
         }
     }
+
+    onDelete = (id) => {
+        let filterArr = this.state.movies.filter(movieObj => {
+            return movieObj._id != id
+        }
+        )
+
+        this.setState({
+            movies:filterArr,
+        })
+    }
+
+    handleChange = (e) =>{
+        let val = e.target.value;
+        this.setState({currentSearchText:val});
+    }
     render() {
-        
+
         return (
             <div className="container">
-            <div className='row'>
-                <div className="col-3">
-                   <h1>Hello</h1>
+                <div className='row'>
+                    <div className="col-3">
+                        <h1>Hello</h1>
+                    </div>
+                    <div className="col-9">
+                        <input onChange={this.handleChange} type='text'></input>
+                        <table class="table table-success table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Genre</th>
+                                    <th scope="col">Stock</th>
+                                    <th scope="col">Rate</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.movies.map(moviesObj => (
+                                        <tr scope='row'>
+                                            <td>{moviesObj.title}</td>
+                                            <td>{moviesObj.genre.name}</td>
+                                            <td>{moviesObj.numberInStock}</td>
+                                            <td>{moviesObj.dailyRentalRate}</td>
+                                            <td><button onClick={() => this.onDelete(moviesObj._id)} type="button" className="btn btn-outline-danger">Delete</button></td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <div className="col-9">
-                   <input type='text'></input> 
-                    <table class="table table-success table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">Title</th>
-                                <th scope="col">Genre</th>
-                                <th scope="col">Stock</th>
-                                <th scope="col">Rate</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                           {
-                               this.state.movies.map(moviesObj=>(
-                                   <tr scope='row'>
-                                       <td>{moviesObj.title}</td>
-                                       <td>{moviesObj.genre.name}</td>
-                                       <td>{moviesObj.numberInStock}</td>
-                                       <td>{moviesObj.dailyRentalRate}</td>
-                                       <td><button type="button" class="btn btn-outline-danger">Danger</button></td>
-                                    </tr>
-                               ))
-                           }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
             </div>
         )
     }
