b61043abaf9df31452c2686df328607f54fb86b4
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-13 22:14:53 +0530

    added upto table

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -12,13 +12,39 @@ export default class Movies extends Component {
     render() {
         
         return (
+            <div className="container">
             <div className='row'>
                 <div className="col-3">
                    <h1>Hello</h1>
                 </div>
                 <div className="col-9">
-                   <input type="text" /> 
+                   <input type='text'></input> 
+                    <table class="table table-success table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Title</th>
+                                <th scope="col">Genre</th>
+                                <th scope="col">Stock</th>
+                                <th scope="col">Rate</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                           {
+                               this.state.movies.map(moviesObj=>(
+                                   <tr scope='row'>
+                                       <td>{moviesObj.title}</td>
+                                       <td>{moviesObj.genre.name}</td>
+                                       <td>{moviesObj.numberInStock}</td>
+                                       <td>{moviesObj.dailyRentalRate}</td>
+                                       <td><button type="button" class="btn btn-outline-danger">Danger</button></td>
+                                    </tr>
+                               ))
+                           }
+                        </tbody>
+                    </table>
                 </div>
+            </div>
             </div>
         )
     }
