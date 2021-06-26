a2bf44d0900924d803a7a765a49736b4834b6cf8
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-26 12:39:28 +0530

    code changed

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,104 +1,90 @@
 import React, { Component } from 'react'
-import { getMovies } from './MovieService'
-
+import { getMovies } from './getMovies';
 export default class Movies extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             movies: getMovies(),
-            currentSearchText:'',
-            // filterMovies:getMovies(),
+            currSearchText:''
         }
     }
-
-    onDelete = (id) => {
-        let filterArr = this.state.movies.filter(movieObj => {
-            return movieObj._id != id
-        }
-        )
-
-        this.setState({
-            movies:filterArr,
-        })
-    }
-
-    handleChange = (e) =>{
+    handleChange=(e)=>{
         let val = e.target.value;
+        console.log(val);
         this.setState({
-            currentSearchText:val
+            currSearchText:val
         })
-        // if(val == '')
-        // {
-        //     this.setState({
-        //             filterMovies:this.state.movies
-        //             ,currentSearchText:''
-        //         })
-        //         return;
-        // }
-        // let  filteredArr = this.state.movies.filter(movieObj=>{
-        //     let title = movieObj.title.trim().toLowerCase();
-        //     // console.log(title);
-        //     return title.includes(val.toLowerCase());
-        // })
-
-        // this.setState({
-        //     filterMovies:filteredArr,
-        //     currentSearchText:val
-        // })
-        // this.setState({currentSearchText:val});
+    }
+    onDelete=(id)=>{
+        let arr =this.state.movies.filter(function(movieObj){
+            return movieObj._id!=id;
+        })
+        // console.log(arr);
+        this.setState({
+            movies:arr
+        });
     }
     render() {
-        let {movies , currentSearchText} = this.state;
-        let filterMovies = [];
-        if(currentSearchText != '')
+        console.log('render');
+        let {movies,currSearchText} =this.state; //ES6 destructuring
+        let filteredArr = [];
+        if(currSearchText=='')
         {
-            filterMovies = movies.filter(movieObj=>{
-                    let title = movieObj.title.trim().toLowerCase();
-                    // console.log(title);
-                    return title.includes(currentSearchText.toLowerCase());
-                })
+            filteredArr = movies;
         }
         else
         {
-            filterMovies = movies;
+            filteredArr = movies.filter(function(movieObj) {
+                let title = movieObj.title.toLowerCase();
+                console.log(title);
+                return title.includes(currSearchText.toLowerCase());
+            })
         }
+       
         return (
-            <div className="container">
+            //JSX
+            <div className='container'>
                 <div className='row'>
-                    <div className="col-3">
-                        <h1>Hello</h1>
+                    <div className='col-3'>
+                        Hello
                     </div>
-                    <div className="col-9">
-                        <input onChange={this.handleChange} type='text'></input>
-                        <table className="table table-success table-striped">
+                    <div className='col-9'>
+                        <input type='search' value={this.state.currSearchText} onChange={this.handleChange} ></input>
+                        <table className="table">
                             <thead>
                                 <tr>
+                                    <th scope="col">#</th>
                                     <th scope="col">Title</th>
                                     <th scope="col">Genre</th>
                                     <th scope="col">
-                                    <i className="fas fa-sort-up"></i>
-                                    Stock
-                                    <i className="fas fa-sort-down"></i>
+                                    <i class="fa fa-sort-asc" aria-hidden="true"></i>
+                                        Stock
+                                        <i class="fa fa-sort-desc" aria-hidden="true"></i>
                                         </th>
                                     <th scope="col">
-                                    <i className="fas fa-sort-up"></i>
-                                    Rate
-                                    <i className="fas fa-sort-down"></i>
-                                    </th>
-                                    <th scope="col"></th>
+                                    <i class="fa fa-sort-asc" aria-hidden="true"></i>
+                                        Rate
+                                        <i class="fa fa-sort-desc" aria-hidden="true"></i>
+                                        </th>
+                                    <th></th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {
-                                    filterMovies.map(moviesObj => (
-                                        <tr scope='row' key={moviesObj._id}>
-                                            <td>{moviesObj.title}</td>
-                                            <td>{moviesObj.genre.name}</td>
-                                            <td>{moviesObj.numberInStock}</td>
-                                            <td>{moviesObj.dailyRentalRate}</td>
-                                            <td><button onClick={() => this.onDelete(moviesObj._id)} type="button" className="btn btn-outline-danger">Delete</button></td>
-                                        </tr>
-                                    ))
+                                    filteredArr.map((movieObj) => {
+                                        return (
+                                            <tr key={movieObj._id} >
+                                                <td></td>
+                                                <td>{movieObj.title}</td>
+                                                <td>{movieObj.genre.name}</td>
+                                                <td>{movieObj.numberInStock}</td>
+                                                <td>{movieObj.dailyRentalRate}</td>
+                                                <td><button onClick={()=>{
+                                                    this.onDelete(movieObj._id)
+                                                }} type="button" className="btn btn-danger">Delete</button></td>
+                                            </tr>
+                                        )
+                                    })
                                 }
                             </tbody>
                         </table>
