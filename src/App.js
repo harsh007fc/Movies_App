8f93079c35d0a42e15132cd6e5968abafba8b79c
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-02 14:51:27 +0530

    added upto little routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Movies from './Components/Movies'
-
+import About from './Components/About';
+import Home from './Components/Home';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 function App() {
   return (
-   <>
-   <Movies/>
-   </>
+   <Router>
+   {/* <Home/> */}
+   <Route path='/' component={Home}/>
+   {/* <Movies/> */}
+   <Route path='/movies' component={Movies}/>
+   {/* <About/> */}
+   <Route path='/about' component={About}/>
+   </Router>
   );
 }
 
