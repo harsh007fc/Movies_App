bc0bc6799f1ec388003535aa589a784cc86c51b7
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-02 22:32:14 +0530

    added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,12 @@ function App() {
       {/* in place of this we use exact keyword */}
    <Route path='/' exact component={Home}/>
    <Route path='/movies' exact component={Movies}/>
-   <Route path='/about' exact component={About}/>
+   {/* agr route ko ese render krvayenge toh prop ni pass krva skte */}
+   {/* <Route path='/about' exact component={About} isAuth={true}/> */}
+
+   <Route path='/about' render={(props)=>(
+     <About {...props} isAuth={true}/>
+   )}/>
    </Switch>
    </Router>
   );
